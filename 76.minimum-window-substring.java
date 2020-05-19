6806ba81be78780aef8bf2affd3bf226830c8a67
Guangxu Li <guangxu.li@nyu.edu>
2020-05-19 17:04:59 -0400

    slide window

---
+++ 76.minimum-window-substring.java
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=76 lang=java
+ *
+ * [76] Minimum Window Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String minWindow(String s, String t) {
+        int[] tCount = new int[256];
+
+        for (int i = 0; i < t.length(); i++) {
+            tCount[t.charAt(i)]++;
+        }
+
+        // index of ans window
+        int start = 0;
+        int end = 0;
+        // ans window size
+        int min = Integer.MAX_VALUE;
+
+        int formed = 0; // number of character in t which has been included in the window
+
+        for (int hi = 0, lo = 0; hi < s.length(); hi++) {
+            if (tCount[s.charAt(hi)]-- > 0) {
+                formed++;
+            }
+
+            while (lo <= hi && formed == t.length()) {
+                if (hi - lo + 1 < min) {
+                    min = hi - lo + 1;
+                    start = lo;
+                    end = hi + 1;
+                }
+
+                // character in s but not in s == -1
+                if (tCount[s.charAt(lo++)]++ == 0) {
+                    formed--;
+                }
+            }
+        }
+
+        return s.substring(start, end);
+    }
+}
+// @lc code=end
