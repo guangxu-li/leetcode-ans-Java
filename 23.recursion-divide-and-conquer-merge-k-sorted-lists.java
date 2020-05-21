e8fbd3fef7d75feb6ba0db3c46d88e4de78fd060
Guangxu Li <guangxu.li@nyu.edu>
2020-05-21 02:00:06 -0400

    recursion divide and conquer

---
+++ 23.recursion-divide-and-conquer-merge-k-sorted-lists.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        } else if (lists.length == 1) {
+            return lists[0];
+        } else {
+            int left = (int) lists.length / 2;
+
+            return mergeTwoLists(mergeKLists(Arrays.copyOfRange(lists, 0, left)),
+                    mergeKLists(Arrays.copyOfRange(lists, left, lists.length)));
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode prev = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
+}
+// @lc code=end
