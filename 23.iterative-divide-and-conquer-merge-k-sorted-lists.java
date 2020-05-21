b248331718083991b088a081198a8b8a05afd3df
Guangxu Li <guangxu.li@nyu.edu>
2020-05-21 01:59:38 -0400

    iterative

---
+++ 23.iterative-divide-and-conquer-merge-k-sorted-lists.java
@@ -0,0 +1,59 @@
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        int size = lists.length; // how many lists unmerged
+        while (size != 1) {
+            int lo = 0;
+            int hi = size - 1;
+            while (lo < hi) {
+                lists[lo] = mergeTwoLists(lists[lo++], lists[hi--]);
+            }
+
+            size = hi + 1;
+        }
+
+        return lists[0];
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode prev = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
+}
+// @lc code=end
