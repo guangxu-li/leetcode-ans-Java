f8fc945569b2e6e7fd66e14aa8a4fdbfdd96264e
Guangxu Li <guangxu.li@nyu.edu>
2020-05-21 01:59:44 -0400

    merge one by one

---
+++ 23.merge-one-by-one-merge-k-sorted-lists.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int i = 1;
+        for (i = 1; i < lists.length; i++) {
+            lists[i] = mergeTwoLists(lists[i - 1], lists[i]);
+        }
+
+        return lists.length == 0 ? null : lists[--i];
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(-1);
+        ListNode prev = head;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+
+            prev = prev.next;
+        }
+
+        prev.next = l1 == null ? l2 : l1;
+
+        return head.next;
+    }
+}
+// @lc code=end
