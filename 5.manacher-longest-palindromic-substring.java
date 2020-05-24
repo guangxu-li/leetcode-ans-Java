2abe468b845a6f1fb7c7ee13a5711e0299e60a5a
Guangxu Li <guangxu.li@nyu.edu>
2020-05-24 18:25:52 -0400

    UPDATE: comments

--- 5.manacher-longest-palindromic-substring.java
+++ 5.manacher-longest-palindromic-substring.java
@@ -7,6 +7,7 @@
 // @lc code=start
 class Solution {
     public String longestPalindrome(String s) {
+        // code line +8 to line +15
         int strLen = 2 * s.length() + 3;
         char[] sChars = new char[strLen];
 
@@ -27,21 +28,24 @@ class Solution {
         int start = 0;
         int maxRight = 0;
         int center = 0;
-        int[] p = new int[strLen];
+        int[] p = new int[strLen]; // i's radius, which not includes i
         for (int i = 1; i < strLen - 1; i++) {
             if (i < maxRight) {
                 p[i] = Math.min(maxRight - i, p[2 * center - i]);
             }
 
+            // expand center
             while (sChars[i + p[i] + 1] == sChars[i - p[i] - 1]) {
                 p[i]++;
             }
 
+            // update center and its bound
             if (i + p[i] > maxRight) {
                 center = i;
                 maxRight = i + p[i];
             }
 
+            // update ans
             if (p[i] > maxLen) {
                 start = (i - p[i] - 1) / 2;
                 maxLen = p[i];
