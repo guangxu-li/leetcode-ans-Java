c93cde616575f921d12751ef3478656a5c6befae
Guangxu Li <guangxu.li@nyu.edu>
2020-05-24 23:32:57 -0400

    CREATE: problem 6, visit by row

---
+++ 6.visit-by-row-zig-zag-conversion.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=6 lang=java
+ *
+ * [6] ZigZag Conversion
+ */
+
+// @lc code=start
+class Solution {
+    public String convert(String s, int numRows) {
+        char[] charArray = s.toCharArray();
+        int len = charArray.length;
+        StringBuilder ans = new StringBuilder();
+
+        if (numRows == 1) {
+            return s;
+        }
+
+        for (int i = 0; i < Math.min(len, numRows); i++) {
+            int inteval = 2 * numRows - 2 - 2 * i;
+            ans.append(charArray[i]);
+            for (int j = inteval + i; j < len; j = j + inteval) {
+                if (inteval != 0) {
+                    ans.append(charArray[j]);
+                }
+
+                inteval = 2 * numRows - 2 - inteval;
+            }
+        }
+
+        return ans.toString();
+    }
+}
+// @lc code=end
