30fe1069f3021530ee7a39d2a0b2fd1001bc3e60
Guangxu Li <guangxu.li@nyu.edu>
2020-05-25 00:15:25 -0400

    UPDATE: update variables' name

--- 7.reverse-integer.java
+++ 7.reverse-integer.java
@@ -7,23 +7,22 @@
 // @lc code=start
 class Solution {
     public int reverse(int x) {
-        int ans = 0;
+        int rev = 0;
         while (x != 0) {
-            int tmp = x % 10;
-            x = x / 10;
+            int pop = x % 10;
+            x /= 10;
 
             // * Integer.MAX_VALUE =  2147483647
             // * Integer.MIN_VALUE = -2147483648
-            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10) && tmp > 7) {
+            if (rev > Integer.MAX_VALUE / 10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {
                 return 0;
-            } else if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10) && tmp < -8) {
+            } else if (rev < Integer.MIN_VALUE / 10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {
                 return 0;
             } else {
-                ans = 10 * ans + tmp;
+                rev = rev * 10 + pop;
             }
         }
-
-        return ans;
+        return rev;
     }
 }
 // @lc code=end
