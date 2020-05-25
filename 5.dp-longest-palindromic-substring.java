036424172f4f844dd60ee2550e23c8e97342f9ef
Guangxu Li <guangxu.li@nyu.edu>
2020-05-24 21:26:44 -0400

    UPDATE: optimize code and reduce the space to O(n)

--- 5.dp-longest-palindromic-substring.java
+++ 5.dp-longest-palindromic-substring.java
@@ -10,7 +10,10 @@ class Solution {
         char[] sChars = s.toCharArray();
 
         int m = s.length();
-        boolean[][] dp = new boolean[m][m];
+
+        // * dp[i][len + 1] means substring starting from i with length of len;
+        boolean[][] dp = new boolean[m][2];
+        int currCol = 0;
 
         int maxLen = 0;
         int ans = 0; // record start index of s
@@ -19,18 +22,20 @@ class Solution {
             for (int start = 0; start + len < m; start++) {
                 int end = start + len;
                 if (len == 0) {
-                    dp[start][end] = true;
+                    dp[start][currCol] = true;
                 } else if (len == 1) {
-                    dp[start][end] = (sChars[start] == sChars[end]);
+                    dp[start][currCol] = (sChars[start] == sChars[end]);
                 } else {
-                    dp[start][end] = (sChars[start] == sChars[end] && dp[start + 1][end - 1]);
+                    dp[start][currCol] = (sChars[start] == sChars[end] && dp[start + 1][currCol]);
                 }
 
-                if (dp[start][end] && len + 1 > maxLen) {
+                if (dp[start][currCol] && len + 1 > maxLen) {
                     ans = start;
                     maxLen = len + 1;
                 }
             }
+
+            currCol = 1 - currCol;
         }
 
         return maxLen == 0 ? "" : s.substring(ans, ans + maxLen);
