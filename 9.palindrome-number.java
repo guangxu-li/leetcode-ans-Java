951db4d2deea4106d5fb9ac869fe4cdc51a222a2
Guangxu Li <guangxu.li@nyu.edu>
2020-05-25 09:34:55 -0400

    CREATE: problem 9 revert half of the number

---
+++ 9.palindrome-number.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0) && x != 0) {
+            return false;
+        }
+
+        int rev = 0;
+        while (x > rev) {
+            rev = 10 * rev + x % 10;
+            x /= 10;
+        }
+
+        return rev == x || rev / 10 == x;
+    }
+}
+// @lc code=end
