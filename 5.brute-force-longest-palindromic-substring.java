c1d167aaf93f4ddda94285e03de3eb90e73505c5
Guangxu Li <guangxu.li@nyu.edu>
2020-05-24 21:27:15 -0400

    CREATE: problem 5 with brute force method

---
+++ 5.brute-force-longest-palindromic-substring.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        char[] charArray = s.toCharArray();
+        int start = 0;
+        int maxLen = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            for (int len = 0; i + len < charArray.length; len++) {
+                if (isPalindrome(charArray, i, len) && len + 1 > maxLen) {
+                    maxLen = len + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+
+    public boolean isPalindrome(char[] charArray, int start, int len) {
+        while (len > 0) {
+            if (charArray[start] != charArray[start + len]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "baac";
+        System.out.println(longestPalindrome(s));
+    }
+}
+// @lc code=end
