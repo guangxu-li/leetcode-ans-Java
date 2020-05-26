e80bd0eeed42e008c76c78ae92a476baa6911a9d
Guangxu Li <guangxu.li@nyu.edu>
2020-05-26 00:06:02 -0400

    CREATE: 10. dp bottom up

---
+++ 10.dp-bottom-up-regular-expression-matching.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=10 lang=java
+ *
+ * [10] Regular Expression Matching
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[s.length()][p.length()] = true;
+
+        for (int i = s.length(); i >= 0; i--) {
+            for (int j = p.length() - 1; j >= 0; j--) {
+                boolean firstMatch = i != s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+
+                if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                } else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+}
+// @lc code=end
