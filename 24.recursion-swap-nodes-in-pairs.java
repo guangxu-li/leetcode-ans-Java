1ae931b23f6f19cec437d7f2bb15b3fb9bf02303
Guangxu Li <guangxu.li@nyu.edu>
2020-05-29 00:08:10 -0400

    CREATE: problem 24 recursion

---
+++ 24.recursion-swap-nodes-in-pairs.java
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=24 lang=java
+ *
+ * [24] Swap Nodes in Pairs
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode l = head;
+        ListNode r = head.next;
+
+        l.next = swapPairs(r.next);
+        r.next = l;
+
+        return r;
+    }
+}
+// @lc code=end
