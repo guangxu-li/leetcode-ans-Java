0123be8dd481ec9a79751932cd82ea68a0bb3836
Guangxu Li <guangxu.li@nyu.edu>
2020-05-30 12:07:53 -0400

    OPTIMIZE: reduce space complexity to O(P)

--- 10.dp-bottom-up-regular-expression-matching.java
+++ 10.dp-bottom-up-regular-expression-matching.java
@@ -7,22 +7,29 @@
 // @lc code=start
 class Solution {
     public boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-        dp[s.length()][p.length()] = true;
+        boolean[][] dp = new boolean[2][p.length() + 1];
 
+        int currRow = 0;
         for (int i = s.length(); i >= 0; i--) {
-            for (int j = p.length() - 1; j >= 0; j--) {
+            for (int j = p.length(); j >= 0; j--) {
+                if (j == p.length()) {
+                    dp[currRow][j] = i == s.length();
+                    continue;
+                }
+
                 boolean firstMatch = i != s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
 
                 if (j < p.length() - 1 && p.charAt(j + 1) == '*') {
-                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                    dp[currRow][j] = dp[currRow][j + 2] || (firstMatch && dp[1 - currRow][j]);
                 } else {
-                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                    dp[currRow][j] = firstMatch && dp[1 - currRow][j + 1];
                 }
             }
+
+            currRow = 1 - currRow;
         }
 
-        return dp[0][0];
+        return dp[1 - currRow][0];
     }
 }
 // @lc code=end
