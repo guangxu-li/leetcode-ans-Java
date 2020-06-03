a4517ff7b467e9f2e22fb25dcc1aea9b449fa96a
Guangxu Li <guangxu.li@nyu.edu>
2020-06-02 22:00:50 -0400

    CREAT: binary search

---
+++ 35.search-insert-position.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=35 lang=java
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
+
+        while (lo <= hi) {
+            // ! there's a possibility of (lo + hi) causes overflow in Java
+            // int mid = (lo + hi) / 2;
+            // int mid = lo + (hi - lo) / 2;
+            int mid = (lo + hi) >> 1;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+
+        return lo;
+    }
+}
+// @lc code=end
