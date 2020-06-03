f5fe9e9b4d3a45a6ea2ff4938b8546d792d996a2
Guangxu Li <guangxu.li@nyu.edu>
2020-06-02 22:00:37 -0400

    CREATE: linear scan

---
+++ 34.linear-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=34 lang=java
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ */
+
+// @lc code=start
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = { -1, -1 };
+
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                if (curr == 0) {
+                    ans[curr] = i;
+                    curr = 1 - curr;
+                }
+                ans[curr] = i;
+            }
+        }
+
+        return ans;
+    }
+}
+// @lc code=end
