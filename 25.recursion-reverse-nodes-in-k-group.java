75e67482ef4179e057d2f363a3ddba2aa36c0784
Guangxu Li <guangxu.li@nyu.edu>
2020-06-08 13:55:09 -0400

    style: add comments

--- 25.recursion-reverse-nodes-in-k-group.java
+++ 25.recursion-reverse-nodes-in-k-group.java
@@ -20,21 +20,28 @@ class Solution {
         ListNode h = head;
         int size = 0;
 
+        // get number of nodes
         while (h != null) {
             size++;
             h = h.next;
         }
 
+        // no need to reverse
         if (size < k) {
             return head;
         }
 
+        // move to the head of next group
         ListNode nextHead = head;
 
         for (int i = 0; i < k; i++) {
             nextHead = nextHead.next;
         }
 
+        /*
+         * 1. reverse current group
+         * 2. reverse remaining nodes
+         */
         h = reverseKNodes(head, k);
         head.next = reverseKGroup(nextHead, k);
 
@@ -42,17 +49,24 @@ class Solution {
     }
 
     public ListNode reverseKNodes(ListNode head, int count) {
+        // no need to reverse
         if (count < 2) {
             return head;
         }
 
+        // initialize pointers position
         ListNode prev = head;
         ListNode curr = head.next;
         prev.next = null;
 
         while (count > 1) {
+            // keep next node
             ListNode next = curr.next;
+
+            // reverse
             curr.next = prev;
+
+            // move to the next
             prev = curr;
             curr = next;
 
