ccc5a47598c54b804ac7525e645ed5ea1708c30a
Guangxu Li <guangxu.li@nyu.edu>
2020-06-20 17:28:38 -0400

    feat(p73): add func to set 0's row and col to 0

---
+++ 73.set-matrix-zeroes.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode id=73 lang=java
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRowIsZero = false;
+
+        for (int num : matrix[0]) {
+            firstRowIsZero = num == 0 ? true : firstRowIsZero;
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = matrix[0].length - 1; j >= 0; j--) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRowIsZero) {
+            Arrays.fill(matrix[0], 0);
+        }
+    }
+}
+// @lc code=end
+
