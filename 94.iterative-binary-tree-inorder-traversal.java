98159bd772f5ed241db61afd71f425c52fe1f1c8
Guangxu Li <guangxu.li@nyu.edu>
2020-06-25 22:36:29 -0400

    feat: add func to in order traverse binary tree

---
+++ 94.iterative-binary-tree-inorder-traversal.java
@@ -0,0 +1,50 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=94 lang=java
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> vals = new ArrayList<>();
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+
+            node = stack.pop();
+            vals.add(node.val);
+            node = node.right;
+        }
+
+        return vals;
+    }
+}
+// @lc code=end
+
