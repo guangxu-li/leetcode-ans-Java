90731704e95d8b23892e2bded1c5dff8fa91a5fe
Guangxu Li <guangxu.li@nyu.edu>
2020-07-10 15:46:23 -0400

    feat: do the sum of two number list

---
+++ 445.add-two-numbers-ii.java
@@ -0,0 +1,53 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * @lc app=leetcode id=445 lang=java
+ *
+ * [445] Add Two Numbers II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Deque<Integer> s1 = new ArrayDeque<>();
+        Deque<Integer> s2 = new ArrayDeque<>();
+
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+
+        int sum = 0;
+        ListNode list = new ListNode(0);
+        while (!s1.isEmpty() || !s2.isEmpty()) {
+            sum += s1.isEmpty() ? 0 : s1.pop();
+            sum += s2.isEmpty() ? 0 : s2.pop();
+            list.val = sum % 10;
+
+            ListNode head = new ListNode(sum / 10);
+            head.next = list;
+            list = head;
+            sum /= 10;
+        }
+
+        return list.val == 0 ? list.next : list;
+    }
+}
+// @lc code=end
+
