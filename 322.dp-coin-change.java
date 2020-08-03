b94055ebae40f57f5b4341b51b4e3599f6c9c19b
Guangxu Li <guangxu.li@nyu.edu>
2020-08-03 12:24:36 -0400

    feat: coin change

---
+++ 322.dp-coin-change.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=322 lang=java
+ *
+ * [322] Coin Change
+ */
+
+// @lc code=start
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = -1;
+            for (int coin : coins) {
+                if (i >= coin) {
+                    if (dp[i - coin] != -1) {
+                        dp[i] = dp[i] == -1 ? 1 + dp[i - coin] : Math.min(dp[i], 1 + dp[i - coin]);
+                    }
+                }
+            }
+        }
+
+        return dp[amount];
+    }
+}
+// @lc code=end
+
