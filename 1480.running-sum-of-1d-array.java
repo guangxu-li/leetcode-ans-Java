e1c2b232822544097abd0d92546b084eeb4edcb8
Guangxu Li <guangxu.li@nyu.edu>
2020-08-17 07:22:36 -0400

    feat: initial running sum

---
+++ 1480.running-sum-of-1d-array.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=1480 lang=java
+ *
+ * [1480] Running Sum of 1d Array
+ */
+
+// @lc code=star
+class Solution {
+    public int[][] highFive(int[][] items) {
+
+    }
+}
+// @lc code=end
+
