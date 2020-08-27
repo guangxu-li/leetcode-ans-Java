140984f3ef10e087ee23829d63d76a21b80ebfdb
Guangxu Li <guangxu.li@nyu.edu>
2020-08-26 21:44:34 -0400

    feat: diameter of binary tree

---
+++ 501-600/543.diameter-of-binary-tree.java
@@ -0,0 +1,48 @@
+import javax.swing.tree.TreeNode;
+
+/*
+ * @lc app=leetcode id=543 lang=java
+ *
+ * [543] Diameter of Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int ans = 0;
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int l = dfs(root.left);
+        int r = dfs(root.right);
+
+        ans = Math.max(ans, l + r);
+
+        return Math.max(l, r) + 1;
+    }
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+
+        return ans;
+    }
+}
+// @lc code=end
+
