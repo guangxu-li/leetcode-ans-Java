abee8c66486d579341eaac1958ec076fbaa7f055
Guangxu Li <guangxu.li@nyu.edu>
2020-08-30 01:13:12 -0400

    feat: largest number

---
+++ 101-200/179.largest-number.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/*
+ * @lc app=leetcode id=179 lang=java
+ *
+ * [179] Largest Number
+ */
+
+// @lc code=start
+class Solution {
+    public String largestNumber(int[] nums) {
+        String max = Arrays.stream(nums).mapToObj(Integer::toString)
+                .sorted((a, b) -> (b + a).compareTo(a + b)).collect(Collectors.joining());
+
+        return max.charAt(0) == '0' ? "0" : max;
+    }
+}
+// @lc code=end
+
